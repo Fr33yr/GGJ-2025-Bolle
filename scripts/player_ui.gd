73f9c3dbf683f0ffa068f34b06bed376bfaa9db2
fr33yr <fr33yr@gmail.com>
2025-01-28 17:50:29 -0300

    CountdownTimer timer as child of the player node ui for the timer when the timer reaches the mark of 120 you win the game and move to the victory scene

--- scripts/player_ui.gd
+++ scripts/player_ui.gd
@@ -2,16 +2,27 @@ extends Control
 
 @onready var player = $"../../Player"
 @onready var life_ui_container = $HBoxContainer
+@onready var count_downlabel = $Time
+@onready var countdown_timer = $CountdownTimer
+
 var hp_system : Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	hp_system = player.get_node("HP_System")
 	hp_system.damage_taken.connect(lost_a_life)	
+	countdown_timer.timeout.connect(_on_countdown_timeout)
+	update_countdown_label()
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	update_countdown_label()
 
 func lost_a_life(current_hp):
 	var last_child = life_ui_container.get_child(life_ui_container.get_child_count()-1)
 	life_ui_container.remove_child(last_child)
+	
+func _on_countdown_timeout():
+	get_tree().change_scene_to_file("res://scenes/victory.tscn")
+
+func update_countdown_label(): # Updates time label, time in seconds
+	count_downlabel.text = "TIME " + str(int(countdown_timer.time_left)) 
